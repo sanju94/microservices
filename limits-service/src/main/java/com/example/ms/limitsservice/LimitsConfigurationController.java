c55331d8a2a74e933a85a6445579f1d45b7ea1ba
Deepak <deepaknayak42@gmail.com>
2019-05-11 23:29:52 +0530

    third file

---
+++ limits-service/src/main/java/com/example/ms/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.example.ms.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ms.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retreiveLimitsFromConfiguration() {
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+}
